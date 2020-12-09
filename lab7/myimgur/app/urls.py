d75608ead1189901ff9a5513c330eb944054f187
levara <levara@levara.xyz>
2020-12-09 11:36:52 +0100

    Add starter lab7
    change base.html

---
+++ lab7/myimgur/app/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'app'
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('<int:image_id>/', views.detail, name='detail'),
+    path('<int:image_id>/comment', views.comment, name='comment'),
+]
