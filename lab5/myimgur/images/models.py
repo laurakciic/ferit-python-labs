49c818b3b3955917053b8f1b7b9464c7dc86eb1e
laura <laurakciic@gmail.com>
2021-11-20 10:24:54 +0100

    add image urls and comments

--- lab5/myimgur/images/models.py
+++ lab5/myimgur/images/models.py
@@ -5,13 +5,15 @@ class Image(models.Model):
     title = models.CharField(max_length = 255, unique = True, blank = False)
     url = models.CharField(max_length = 512, blank = False)
     pub_date = models.DateTimeField('Published at')
-
+    def __str__(self):
+        return "%d. %s"%(self.id, self.title)
 
 class Comment(models.Model):
     image = models.ForeignKey(Image, on_delete = models.CASCADE)
     text = models.TextField(blank = False)
     nick = models.CharField(max_length = 128, blank = False)
-    
+    def __str__(self):
+        return "%d. %s"%(self.id, self.nick)
 
 
 
