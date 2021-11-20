034df3da7fa0af8068a1054cbf690379dd988291
laura <laurakciic@gmail.com>
2021-11-20 10:12:31 +0100

    add Image and Comment models

--- lab5/myimgur/images/models.py
+++ lab5/myimgur/images/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Image(models.Model):
+    title = models.CharField(max_length = 255, unique = True, blank = False)
+    url = models.CharField(max_length = 512, blank = False)
+    pub_date = models.DateTimeField('Published at')
+
+
+class Comment(models.Model):
+    image = models.ForeignKey(Image, on_delete = models.CASCADE)
+    text = models.TextField(blank = False)
+    nick = models.CharField(max_length = 128, blank = False)
+    
+
+
+
+
