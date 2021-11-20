c729595b75f4b302e728dfc0796e36cd3b3f9321
laura <laurakciic@gmail.com>
2021-11-20 09:25:32 +0100

    add html basic implementation

--- lab5/myimgur/images/views.py
+++ lab5/myimgur/images/views.py
@@ -4,9 +4,14 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    txt = 'Radi'
-    return HttpResponse(txt)
+    # txt = 'Radi'
+    # return HttpResponse(txt)
+    context = {}
+    return render(request, 'images/index.html', context)
+
 
 def about(request):
-    txt = "Laura"
-    return HttpResponse(txt)
+    # txt = "Laura"
+    # return HttpResponse(txt)
+    context = {}
+    return render(request, 'images/about.html', context)
