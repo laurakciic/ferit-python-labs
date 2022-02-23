9adbef34ea0d0f2bd5fbcc5a6036a9bf2ebf560c
levara <hrvoje.leventic@gmail.com>
2022-02-23 13:53:59 +0100

    Add 20220223 racunarstvo exam

---
+++ ispiti/racunarstvo/20220223/myimgur/app/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from .models import Image, Comment, Vote
+
+class CommentInline(admin.StackedInline):
+    model = Comment
+    extra = 0
+
+class ImageAdmin(admin.ModelAdmin):
+    inlines = [CommentInline]
+    #fields = ['title', 'url', 'pub_date', 'description']
+
+# Register your models here.
+admin.site.register(Image, ImageAdmin)
+admin.site.register(Comment)
+admin.site.register(Vote)
